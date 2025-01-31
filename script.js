043bc87b15cd646b7cabef0faae2b2924059d7cf
Arivonil <arivonilsampaio@users.noreply.github.com>
2025-01-31 20:28:48 -0300

    31-01-25

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 $(document).ready(() => {
-	
+
+  $('#documentacao').on('click', () => {
+    $('#pagina').load('documentacao.html')
+
+    // $.get('documentacao.html', data => {
+    //   $('#pagina').html(data)
+    // })
+
+  })
+
+  $('#suporte').on('click', () => {
+    $('#pagina').load('suporte.html')
+
+    // $.get('suporte.html', data => {
+    //   $('#pagina').html(data)
+    // })
+
+  })
+
+
+  // AJAX
+  //ajax
+  $('#competencia').on('change', e => {
+
+    // console.log($(e.target).val())
+    
+    let competencia =  $(e.target).val()
+
+    $.ajax({
+      type:'GET',
+      url: 'app.php',
+      data: `competencia=${competencia}`,    //x-www-form-urlencode
+      dataType: 'json',
+      success: dados => {
+        $('#mumeroVendas').html(dados.numeroVendas),
+        $('#totalVendas').html(dados.totalVendas)
+      },
+      error: erro => {console.log(erro)}
+      
+    })
+    
+    //   //método, url, dados, sucesso, erro
+    // 
+  })
+
 })
